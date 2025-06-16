2d1b807af6e82c7ced23b42383c673a45ec32490
AnkitaBhandari1234 <ankitabhandari376@gmail.com>
2025-06-16 16:12:05 +0545

    here

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
